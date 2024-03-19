8ec72e716a5309c1f1fcfee62d7c25a4a5f57bac
Viacheslav <163991845+736C617661@users.noreply.github.com>
2024-03-19 11:25:34 +0200

    Add files via upload

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,4 +1,19 @@
-int main(void) {
-float x = 0.0;
-float y = 0.0;
-y = pow(x,
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x; // Змінна для вводу значення x
+    double y; // Змінна для зберігання результату
+
+    // Запит на введення значення x
+    printf("Введіть значення x: ");
+    scanf("%lf", &x);
+
+    // Обчислення значення функції
+    y = pow(x, 3) - 4 * pow(x, 2) - 5 * x + 9 + cos(x);
+
+    // Виведення результату
+    printf("При x = %f, y = %f\n", x, y);
+
+    return 0;
+}
