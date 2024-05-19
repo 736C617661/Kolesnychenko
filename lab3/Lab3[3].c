18b5f09e7ea73a84bf1c597a22cedd2871dbb887
Viacheslav <163991845+736C617661@users.noreply.github.com>
2024-05-19 15:23:14 +0300

    Update Lab3[3].c

--- lab3/Lab3[3].c
+++ lab3/Lab3[3].c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    // Припустимо, що у нас є словник з номерами серій як ключами та відповідними назвами та датами прем'єр як значеннями
-    // Приклад даних для серіалу "El Candidato"
     int episode_number;
     char *title;
     char *premiere_date;
 
-    // Введіть номер серії
     printf("Введіть номер серії: ");
     scanf("%d", &episode_number);
 
-    // Перевірте, чи існує номер серії в даних
     switch (episode_number) {
         case 0:
             title = "Trailer";
@@ -57,13 +53,13 @@ int main() {
             title = "Серія 10";
             premiere_date = "17-07-2020";
             break;
-        // Додайте більше серій за потреби
+        
         default:
             printf("Серію з номером %d не знайдено в даних.\n", episode_number);
             return 1;
     }
 
-    // Виведіть назву та дату прем'єрного показу
+    
     printf("%d: %s (Дата прем'єри: %s)\n", episode_number, title, premiere_date);
 
     return 0;
