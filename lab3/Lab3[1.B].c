d93e6cdc7bcbdc9ca6df1f06b191438067153b72
Viacheslav <163991845+736C617661@users.noreply.github.com>
2024-05-20 18:52:10 +0300

    Update Lab3[1.B].c

--- lab3/Lab3[1.B].c
+++ lab3/Lab3[1.B].c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
-double calculate_f_full(double x) {
-    double result;
-    if (x < -10) {
-        result = 3 * pow(x, 3) - pow(x, 2);
+double f(double x) {
+    if (x < 10) {
+        return 3 * pow(x, 2) - pow(x, 3);
+    } else if (-10 <= x && x <= 10) {
+        return sqrt(10 - x);
     } else {
-        if (-10 <= x && x <= 10) {
-            result = sqrt(10 - x);
-        } else {
-            result = 2 * x + 1;
-        }
+        return 2 * x + 1;
     }
-    return result;
 }
 
 int main() {
-    double x_value;
-    printf("Введіть значення x: ");
-    scanf("%lf", &x_value);
-
-    double result_full = calculate_f_full(x_value);
-    printf("f(%.2lf) = %.2lf\n", x_value, result_full);
-
+    double x;
+    printf("Введіть число x: ");
+    scanf("%lf", &x);
+    printf("Результат: %f\n", f(x));
     return 0;
 }
+
